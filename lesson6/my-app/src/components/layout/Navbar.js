d6f95c56bbb140a92f1ec337ae5030d969665bd3
patbi <biyagapatrick@gmail.com>
2023-06-14 23:59:56 +0100

    lesson6 update1

---
+++ lesson6/my-app/src/components/layout/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="header">
+            <h1> useState Project </h1>
+         </div>
+    )
+}
+export default Navbar;
