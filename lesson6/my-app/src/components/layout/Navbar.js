07c0c1c8f071ac08fc46ec32edfff4f6a50fd6f8
patbi <biyagapatrick@gmail.com>
2023-06-16 17:28:05 +0100

    useState Form Example

--- lesson6/my-app/src/components/layout/Navbar.js
+++ lesson6/my-app/src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <div className="header">
-            <h1> useState Project </h1>
+            <h1> useState Form Example </h1>
          </div>
     )
 }
